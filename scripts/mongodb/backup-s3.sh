c5008e9b3263da838b519af09211bb8bc83095bd
odra <lrossett@redhat.com>
2018-04-13 05:20:10 -0300

    gpg key params

--- scripts/mongodb/backup-s3.sh
+++ scripts/mongodb/backup-s3.sh
@@ -8,17 +8,23 @@ USER=$3
 PASSWORD=$4
 AUTH_DB=$5
 S3_BUCKET_NAME=$6
+GPG_RECIPIENT=$7
 
 DATESTAMP=$(date '+%Y-%m-%d')
 
 # Retrieve a list of all databases
 DATABASES=$(mongo $HOST:$PORT/admin -u $USER -p $PASSWORD --eval  "printjson(db.adminCommand('listDatabases'))" | grep name | cut -d '"' -f4 | grep -v local)
 
+if [[ ! -z $GPG_RECIPIENT ]]; then
+  gpg --import /opt/rh/secrets/gpg_public_key
+  gpg --import /opt/rh/secrets/gpg_secret_key
+fi
+
 # For each database archive data and push directly to S3
 for DATABASE in $DATABASES; do
   TIMESTAMP=$(date '+%H:%M:%S')
-  echo "==> Dumping database $DATABASE to S3 bucket s3://$S3_BUCKET_NAME/backups/mysql/$DATESTAMP/"
-  mongodump -h $HOST:$PORT -u $USER -p $PASSWORD -d $DATABASE --archive --gzip --authenticationDatabase $AUTH_DB | aws s3 cp - s3://$S3_BUCKET_NAME/backups/mongodb/$DATESTAMP/$DATABASE-$TIMESTAMP.dump.gz
+  echo "==> Dumping database $DATABASE to S3 bucket s3://$S3_BUCKET_NAME/backups/mongodb/$DATESTAMP/"
+  mongodump -h $HOST:$PORT -u $USER -p $PASSWORD -d $DATABASE --archive --gzip --authenticationDatabase $AUTH_DB | gpg --encrypt --recipient "$GPG_RECIPIENT" --trust-model $GPG_TRUST_MODEL | aws s3 cp - s3://$S3_BUCKET_NAME/backups/mongodb/$DATESTAMP/$DATABASE-$TIMESTAMP.dump.gz
   STATUS=$?
   if [ $STATUS -eq 0 ]; then
     echo "==> Dump $DATABASE: COMPLETED"
